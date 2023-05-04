63261988058c3e5c84c8d441c633fdb77f685fe9
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2023-05-04 12:51:15 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,14 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(avg_dtime)
 })
 input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.SmallSquare)
+    datalogger.log(
+    datalogger.createCV("dtime", avg_dtime),
+    datalogger.createCV("datapoints", data_points)
+    )
+    data_points = 0
+    avg_dtime = 0
+    basic.pause(100)
     radio.sendString("head_tilt_reset")
 })
 radio.onReceivedValue(function (name, value) {
@@ -17,3 +25,4 @@ let data_points = 0
 radio.setGroup(72)
 data_points = 0
 avg_dtime = 0
+datalogger.includeTimestamp(FlashLogTimeStampFormat.Milliseconds)
