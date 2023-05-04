51c0cc25665c85da82c87b8267d255be41d1a3c1
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2023-05-04 00:41:11 -0500

    Adding recording of device downtime to get average across devices

--- main.ts
+++ main.ts
@@ -1,7 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(avg_dtime)
+})
 input.onButtonPressed(Button.AB, function () {
     radio.sendString("head_tilt_reset")
 })
-radio.setGroup(72)
-basic.forever(function () {
-	
+radio.onReceivedValue(function (name, value) {
+    if (name == "dtime") {
+        temp = value + avg_dtime * data_points
+        data_points += 1
+        avg_dtime = temp / data_points
+    }
 })
+let temp = 0
+let avg_dtime = 0
+let data_points = 0
+radio.setGroup(72)
+data_points = 0
+avg_dtime = 0
